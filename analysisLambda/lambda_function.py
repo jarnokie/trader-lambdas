83aa5f83ffdd41c316d03be21dadff17f7339a92
Jarno Kiesiläinen <jarnokie@gmail.com>
2023-08-08 18:15:46 +0300

    Get the symbol name from the request path.

--- analysisLambda/lambda_function.py
+++ analysisLambda/lambda_function.py
@@ -4,9 +4,15 @@ sys.path.insert(0, 'src/vendor/')
 import json
 
 def lambda_handler(event, context):
+    if "pathParameters" in event and "symbol" in event["pathParameters"]:
+        symbol = event["pathParameters"]["symbol"]
+    else:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"message": "symbol is required"})
+        }
     return {
         "statusCode": 200,
-        "body": json.dumps({"message": "hello world",
-                            "event": event})
+        "body": json.dumps({"symbol": symbol})
     }
 
